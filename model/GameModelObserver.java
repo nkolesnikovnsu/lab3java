d1bf80e5a1d5a9d967db162c4eb37ff0803d5928
llya <70789333+ikolesnikov1@users.noreply.github.com>
2021-05-26 22:40:50 +0700

    Delete model directory

--- model/GameModelObserver.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface GameModelObserver {
-    void update(int row, int column, char symbol, String message);
-    void winner(int row, int column, char symbol, String message);
-    void resetGame();
-}
